139575dd5442f9c95ce8326f28123825ff6f6cd8
Mathias R. Jessen <mathias@mrjessen.com>
2017-05-26 00:26:57 +0200

    Initial Commit
    Set-ProxyAlias + basic tests

---
+++ ProxyAlias/Public/Set-ProxyAlias.ps1
@@ -0,0 +1,64 @@
+﻿function Set-ProxyAlias {
+    [CmdletBinding(DefaultParameterSetName='HashTableByName')]
+    param(
+        [Parameter(Mandatory=$true,ParameterSetName='ByCommand',ValueFromPipeline=$true,Position=0)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.CommandInfo]
+        $Command,
+
+        [Parameter(Mandatory=$true,ParameterSetName='ByName',Position=0)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Name,
+
+        [Parameter(Mandatory=$true,Position=1)]
+        [Alias('Alias')]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $NewName,
+
+        [Parameter()]
+        [System.Collections.IDictionary]
+        $Property = @{},
+
+        [Parameter(Position=2)]
+        [string]
+        $PropertyName,
+
+        [Parameter(Position=3)]
+        [Alias('Value')]
+        [psobject]
+        $PropertyValue
+    )
+
+    if($PSCmdlet.ParameterSetName -eq 'ByName'){
+        $Command = Get-Command $Name
+    }
+
+    if($PSBoundParameters.ContainsKey('PropertyName')){
+        if(-not $PSBoundParameters.ContainsKey('PropertyValue'))
+        {
+            $PropertyValue = $true
+        }
+        $Property[$PropertyName] = $PropertyValue
+    }
+
+    try {
+        $Metadata = [System.Management.Automation.CommandMetadata]::new($Command)
+        $ProxyCommand = [System.Management.Automation.ProxyCommand]::Create($Metadata)
+        Set-Item -Path "function:global:$NewName" -Value $ProxyCommand -Force
+    }
+    catch {
+        throw $_
+    }
+
+    $DefaultValues = $PSCmdlet.SessionState.PSVariable.Get('PSDefaultParameterValues').Value
+    foreach($ValuePair in $DefaultValues.Keys |Where-Object {$_ -like "$NewName`:*"}){
+        $DefaultValues.Remove($ValuePair)
+    }
+
+    foreach($PropertyKey in $Property.Keys){
+        $DefaultValues["$NewName`:$PropertyKey"] = $Property[$PropertyKey]
+    }
+    $PSCmdlet.SessionState.PSVariable.Set('PSDefaultParameterValues',$DefaultValues)
+}
---
+++ Tests/Set-ProxyAlias.Tests.ps1
@@ -0,0 +1,29 @@
+﻿$RootModule = Join-Path (Split-Path $PSScriptRoot -Parent) .\ProxyAlias\ProxyAlias.psd1
+
+Remove-Module ProxyAlias -Force -ErrorAction SilentlyContinue
+Import-Module $RootModule
+
+Describe "Set-ProxyAlias" {
+    Context "Input" {
+        It "Takes CommandInfo objects" {
+            {
+                Get-Command Get-Command | Set-ProxyAlias -NewName Get-Syntax -PropertyName Syntax
+            } |Should Not Throw
+        }
+        It "Takes Command Names as strings" {
+            {
+                Set-ProxyAlias -Name Get-Command -NewName Get-Syntax -PropertyName Syntax
+            } |Should Not Throw
+        }
+    }
+    Context "Output" {
+        It "Creates a proxy function" {
+            Get-Command Get-Command | Set-ProxyAlias -NewName Get-Syntax -PropertyName Syntax
+            Get-Command Get-Syntax |Should BeOfType System.Management.Automation.FunctionInfo
+        }
+        It "Sets default parameter values" {
+            Get-Command Get-Command | Set-ProxyAlias -NewName Get-Syntax -PropertyName Syntax
+            $PSDefaultParameterValues['Get-Syntax:Syntax'] |Should Be $true
+        }
+    }
+}
